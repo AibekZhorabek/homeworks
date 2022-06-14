87b0b9900ddc66a4b5afb2e940562acc4bebec74
Aibek Zhorabek <aibekzhorabek.com>
2022-06-14 17:56:39 +0600

    homeworks

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,33 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
-
-  return result; // boolean
+  let l_arr1 = arr1.length;
+  let l_arr2 = arr2.length;
+  if(l_arr1 === l_arr2){
+    for (let i = 0; i < l_arr1; i++) {
+      if (arr1[i] !== arr2[i]){
+        result = false;
+        break;
+      }else
+        result = true;
+    }
+  }else {
+    result = false;
+  }
+  return result;
 }
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  function filterarr(item){
+    let item2 = item * 10
+    if(item2 >= 0 && item2 % 3 === 0)
+      return item2
+  }
+
+  let arr2 = arr.map(item => item * 10)
+  resultArr = arr2.filter(filterarr);
 
   return resultArr; // array
 }
